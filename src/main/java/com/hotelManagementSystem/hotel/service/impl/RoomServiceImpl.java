516d2a073387c469902526de5731b37e3a39d62b
DMGCD <gcdisanayaka1998@gmail.com>
2024-10-29 13:13:37 +0530

    space Change

--- src/main/java/com/hotelManagementSystem/hotel/service/impl/RoomServiceImpl.java
+++ src/main/java/com/hotelManagementSystem/hotel/service/impl/RoomServiceImpl.java
@@ -2,11 +2,9 @@ package com.hotelManagementSystem.hotel.service.impl;
 
 import com.hotelManagementSystem.hotel.model.Room;
 import com.hotelManagementSystem.hotel.service.RoomService;
-import com.hotelManagementSystem.hotel.util.generics.configurations.ModelMapperConfig;
 import com.hotelManagementSystem.hotel.util.generics.dto.RoomSaveDto;
 import com.hotelManagementSystem.hotel.util.generics.repository.RoomRepository;
 import com.hotelManagementSystem.hotel.util.generics.service.impl.CommonServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +12,8 @@ import org.springframework.stereotype.Service;
 @Service
 
 public class RoomServiceImpl extends CommonServiceImpl<Room, Integer, RoomRepository> implements RoomService {
-@Autowired
-   ModelMapper modelMapper;
+    @Autowired
+    ModelMapper modelMapper;
 
 
     public RoomServiceImpl(RoomRepository repository) {
@@ -24,7 +22,7 @@ public class RoomServiceImpl extends CommonServiceImpl<Room, Integer, RoomReposi
 
     @Override
     public String save(RoomSaveDto t) {
-        Room map = modelMapper.map(t,Room.class);
+        Room map = modelMapper.map(t, Room.class);
         repository.save(map);
         return "Save Successful!";
     }
--- src/main/java/com/hotelManagementSystem/hotel/util/generics/configurations/ModelMapperConfig.java
+++ src/main/java/com/hotelManagementSystem/hotel/util/generics/configurations/ModelMapperConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ModelMapperConfig {
     @Bean
-    public ModelMapper modelMapperConfigurations(){
+    public ModelMapper modelMapperConfigurations() {
         return new ModelMapper();
     }
 }
