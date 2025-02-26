8cfdfd60e076969b5371e712c9855f58170a6c4a
gayan <gcdisanayaka1998@gmail.com>
2025-02-26 15:46:59 +0530

    Create Admin Controller And Services For Save The admin

--- src/main/java/com/hotelManagementSystem/hotel/controller/AdminController.java
+++ src/main/java/com/hotelManagementSystem/hotel/controller/AdminController.java
@@ -1,4 +1,24 @@
 package com.hotelManagementSystem.hotel.controller;
 
-public interface AdminController {
+import com.hotelManagementSystem.hotel.model.Admin;
+import com.hotelManagementSystem.hotel.util.generics.controller.CommonController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.List;
+
+public interface AdminController extends CommonController<Admin, Integer> {
+    @GetMapping("/admins")
+    ResponseEntity<List<Admin>> getAll() throws Exception;
+
+    @GetMapping("/admin")
+    ResponseEntity<Admin> findById(Integer id) throws Exception;
+
+    @PutMapping("/update")
+    ResponseEntity<Admin> update(Admin t, Integer id) throws Exception;
+
+    @PostMapping("/create")
+    ResponseEntity<Admin> delete(Integer id) throws Exception;
 }
---
+++ src/main/java/com/hotelManagementSystem/hotel/service/AdminService.java
@@ -0,0 +1,7 @@
+package com.hotelManagementSystem.hotel.service;
+
+import com.hotelManagementSystem.hotel.model.Admin;
+import com.hotelManagementSystem.hotel.util.generics.service.CommonService;
+
+public interface AdminService extends CommonService<Admin, Integer> {
+}
