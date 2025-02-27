33509e22147c31dade5de2a1e8fafa763ef963dc
gayan <gcdisanayaka1998@gmail.com>
2025-02-27 13:07:31 +0530

    Add All Security Configurations For Needed To JWT  Filter Before

--- src/main/java/com/hotelManagementSystem/hotel/configurations/SecurityConfig.java
+++ src/main/java/com/hotelManagementSystem/hotel/configurations/SecurityConfig.java
@@ -1,37 +1,57 @@
 package com.hotelManagementSystem.hotel.configurations;
 
+import com.hotelManagementSystem.hotel.filter.JWTFilter;
+import com.hotelManagementSystem.hotel.service.impl.CommonUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+    private final CommonUserDetailService userDetailService;
+    private final JWTFilter jwtFilter;
+
+    public SecurityConfig(CommonUserDetailService userDetailService, JWTFilter jwtFilter) {
+        this.userDetailService = userDetailService;
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/customer/**").hasRole("CUSTOMER")
-                        .requestMatchers("customer/saveCustomer").hasRole("ADMIN")
-                        .anyRequest()
-                        .authenticated()
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//Add Filter Before
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/customer/**").hasRole("CUSTOMER")
+                                .requestMatchers("customer/saveCustomer").hasRole("ADMIN")
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailService);
+        return provider;
+    }
+
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration
-                .getAuthenticationManager();
+        return configuration.getAuthenticationManager();
     }
 }
