cc9c84414d476eb09908c7001ed9a633f2d9b40f
gayan <gcdisanayaka1998@gmail.com>
2025-03-13 20:19:03 +0530

    changed Auth HTTP Method As a POST

--- src/main/java/com/hotelManagementSystem/hotel/controller/AuthController.java
+++ src/main/java/com/hotelManagementSystem/hotel/controller/AuthController.java
@@ -2,10 +2,10 @@ package com.hotelManagementSystem.hotel.controller;
 
 import com.hotelManagementSystem.hotel.util.generics.dto.auth.LoginDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface AuthController {
-    @GetMapping("/token")
+    @PostMapping("/token")
     ResponseEntity<String> generateToken(@RequestBody LoginDto auth) throws Exception;
 }
